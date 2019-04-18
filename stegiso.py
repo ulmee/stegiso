583fc6a9b5afc0d9df9a6aba91ccfe3c50ce4b97
Ulmee <gmontekrissto@gmail.com>
2019-04-18 18:15:41 +0300

    Small Fixes

--- stegiso.py
+++ stegiso.py
@@ -1,38 +1,38 @@
-#все необходимые импорты
+#делаем все необходимые операции импорта
 import base64
-import time
 import pycdlib
-#подготавливаем наши тексты
-with open('/home/ul/stegist1.txt','rb') as stegist1:# открываем файл
-    print('#Читаем файл')
+#подготавливаем наше содержимое
+with open('/home/ul/stegist1.txt','rb') as stegist1: #открываем файл
+
     for line1 in stegist1.readlines(2):
-        print(line1)#читаем содержимое
+        print(line1) #читаем содержимое
     for line2 in stegist1.readlines(2):
-        print(line2)#читаем содержимое
+        print(line2) #читаем содержимое
 
 enc_line1=base64.b64encode(line1)
 print(enc_line1)
 enc_line2=base64.b64encode(line2)
 print(enc_line2)
-with open('/home/ul/stegist2.txt','rb') as stegist2:
-    print('#Читаем файл')
-    for line3 in stegist2.readlines(2):
+
+with open('/home/ul/stegist2.txt','rb') as stegist2: #открываем файл
+
+    for line3 in stegist2.readlines(2): #читаем содержимое
         print(line3)
-    for line4 in stegist2.readlines(2):
+    for line4 in stegist2.readlines(2): #читаем содержимое
         print(line4)
-enc_line3=base64.b64encode(line3)
+enc_line3=base64.b64encode(line3) #шифруем содержимое
 print(enc_line3)
 enc_line4=base64.b64encode(line4)
 print(enc_line4)
-with open('/home/ul/up.txt','ab') as up:
+
+with open('/home/ul/up.txt','ab') as up: #В файл up записываем закодированные данные
     up.write(enc_line1)
     up.write(enc_line3)
-with open('/home/ul/down.txt','ab') as down:
+with open('/home/ul/down.txt','ab') as down: #В файл down записываем закодированные данные
     down.write(enc_line2)
     down.write(enc_line4)
-time.sleep(5)
 
-#вносим все в структуру iso
+#заносим все в iso
 iso=pycdlib.PyCdlib()
 iso.new(rock_ridge='1.09')
 iso.add_directory(iso_path='/A1',rr_name='a1')
